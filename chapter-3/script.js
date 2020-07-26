ee454ca769163f5526347535abff369b49743fdc
Alley Purcell <zanastar@gmail.com>
2020-07-26 01:20:29 -0500

    Word Replacement Game - Version 1

---
+++ chapter-3/script.js
@@ -0,0 +1,17 @@
+let adjective1 = "bouncy";
+let noun1 = "cow";
+let noun2 = "pizza";
+let adjective2 = "smelly";
+let shape1 = "triangle";
+let place1 = "kitchen";
+
+let song = "Twinkle, twinkle, " + adjective1;
+song += " " + noun1 + ", ";
+song += "how I wonder what you are. ";
+song += "Up above the " + noun2 + " so ";
+song += adjective2;
+song += ", like a " + shape1 + " in the " + place1;
+song += ". Twinkle, twinkle, " + adjective1 + " ";
+song += noun1 + ", how I wonder what you are.";
+
+alert(song);
