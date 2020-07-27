194beb816f1cdc1dcd23975ad81935833a748d4a
Alley Purcell <zanastar@gmail.com>
2020-07-26 19:38:56 -0500

    4.3 Test Averager - Version 2

--- chapter-4/script.js
+++ chapter-4/script.js
@@ -10,3 +10,21 @@ let sum = score1 + score2 + score3;
 alert("The sum is " + sum);
 let average = Math.round(sum / 3);
 alert("Your average is " + average);
+
+if (average === 100){
+  alert("That's perfect!");
+} else if (average >= 100) {
+  alert("Awesome! You went above and Beyond!");
+} else if (average >= 90) {
+  alert("Wow! Great Job!");
+} else if (average >= 80) {
+  alert("Good Job!");
+} else if (average >= 70) {
+  alert("Keep up the good work!");
+} else if (average >= 65){
+  alert("You passed!");
+} else if (average >= 55){
+  alert("Almost, study harder.");
+} else {
+  alert("You didn't pass. Keep studying.");
+}
