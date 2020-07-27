03c50cbd0ac76fb4faa37600bff828363c8c33da
Alley Purcell <zanastar@gmail.com>
2020-07-27 00:39:30 -0500

    6.5 Guess My Number - Version 3

--- chapter-6/script.js
+++ chapter-6/script.js
@@ -1,16 +1,20 @@
 let max = 10;
 let computerPick = Math.floor(Math.random() * max) + 1;
-// console.log(computerPick);
 
-let playerPick = prompt("Pick a number between 1 and " + max + ".");
-playerPick = parseInt(playerPick);
+let keepGuessing = true;
+while (keepGuessing) {
 
-if (playerPick < 1 || playerPick > max){
-  alert('That is an invalid choice!');
-} else if (playerPick === computerPick) {
-  alert('Correct!');
-} else if (playerPick < computerPick) {
-  alert('Your guess is to low.');
-} else if (playerPick > computerPick) {
-  alert('Your guess is to high.');
+  let playerPick = prompt("Pick a number between 1 and " + max + ".");
+  playerPick = parseInt(playerPick);
+  
+  if (playerPick < 1 || playerPick > max){
+    alert('That is an invalid choice!');
+  } else if (playerPick === computerPick) {
+    alert('Correct!');
+    keepGuessing = false;
+  } else if (playerPick < computerPick) {
+    alert('Your guess is to low.');
+  } else if (playerPick > computerPick) {
+    alert('Your guess is to high.');
+  }
 }
