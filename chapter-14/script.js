f2dff0d15a1edd99082fb53afa88fe4a1dd4a367
Alley Purcell <zanastar@gmail.com>
2020-08-02 04:17:40 -0500

    14.3  Tip Calulator - Version 2

--- chapter-14/script.js
+++ chapter-14/script.js
@@ -2,9 +2,10 @@ let explanation = document.getElementById("explanation");
 let input = document.getElementById("input");
 let button = document.getElementById("button");
 let output = document.getElementById("output");
+let select = document.getElementById("service");
 
 // Explanation
-explanation.innerHTML = "Enter teh cost of your meal. ";
+explanation.innerHTML = "Enter the cost of your meal. ";
 explanation.innerHTML += "Then press Calculate. ";
 
 // What does the button do
@@ -12,10 +13,13 @@ button.onclick = calculateTip;
 
 // CalculateTip function
 function calculateTip() {
-  let cost = parseFloat(input.value)
-  let percent = 15;
+  let cost = parseFloat(input.value);
+  let percent = parseFloat(select.value);
   let tip = cost * percent / 100;
   tip = tip.toFixed(2);
   let text = "A " + percent + "% tip is $" + tip;
   output.innerHTML = text;
+  let total = cost + parseFloat(tip);
+  output.innerHTML += "<br>The total cost is $" + total;
 }
+
