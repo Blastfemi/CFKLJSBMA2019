fb366d2e33bb4a1ab973517fc04b31b9ad9bbe30
Alley Purcell <zanastar@gmail.com>
2020-08-02 01:45:01 -0500

    12.2 Treasure Quest - Version 2

--- chapter-12/script.js
+++ chapter-12/script.js
@@ -1,71 +1,79 @@
+// Sets the number of times the game repeats
+// Currently a random number between 2 & 5
+let numTreasures = Math.floor(Math.random() * 4) + 2;
 // Game intro
-let text = "You're on a deserted island, looking for treasure. ";
+let text = "You're on a deserted island, looking for " + numTreasures + " treasure. ";
 text += "You have a special compass that tells you how far away you are from the nearest treasure. ";
 text += "You can travel north, south, east, or west. ";
 text += "Find that treasure! ";
 alert(text);
 
-// Treasure Generator
-let treasures = ["bar of gold", "sapphire necklace", "ruby ring", "platinum coin", "string of pearls", "pair of emerald earrings", "opal bracelet"];
-
-// Select the treasure
-let index = Math.floor(Math.random() * treasures.length);
-let treasure = treasures[index];
-
-// Set position & distance from treasure north & east
-let playerX = Math.floor(Math.random() * 5) + 3;
-let playerY = Math.floor(Math.random() * 5) + 3;
-
-// Random chance to start from the west
-if (Math.random() < 0.5) {
-  playerX = -playerX;
+for(let i = 0; i < numTreasures; i++) {
+  playGame();
 }
+// Convert game to a large function to allow for repeating
+function playGame() {
+  // Treasure Generator
+  let treasures = ["bar of gold", "sapphire necklace", "ruby ring", "platinum coin", "string of pearls", "pair of emerald earrings", "opal bracelet"];
 
-// Random chance to start from the south
-if (Math.random() < 0.5) {
-  playerY = -playerY;
-}
+  // Select the treasure
+  let index = Math.floor(Math.random() * treasures.length);
+  let treasure = treasures[index];
 
-// Track number of steps taken to find the treasure
-let steps = [];
+  // Set position & distance from treasure north & east
+  let playerX = Math.floor(Math.random() * 5) + 3;
+  let playerY = Math.floor(Math.random() * 5) + 3;
 
-// Initial distance from the treasure
-let distance = getDistance(playerX, playerY);
-
-// Game loop
-while (distance !== 0) {
-
-  // Prompt player for move by selecting direction
-  let direction = prompt("You are " + distance + " meters away from the treasure. Will you go north, east, south, or west?");
-
-  // detects direction by taking the 0 index of the players input and then converts to lowercase
-  direction = direction[0].toLowerCase();
-
-  // Directional movement
-  if (direction === "n") {
-    playerY++;
-    steps.push("north")
-  } else if (direction === "s") {
-    playerY--;
-    steps.push("south")
-  } else if (direction === "e") {
-    playerX++;
-    steps.push("east")
-  } else if (direction === "w") {
-    playerX--;
-    steps.push("west")
-  } else {
-    alert("That's an invalid direction.")
+  // Random chance to start from the west
+  if (Math.random() < 0.5) {
+    playerX = -playerX;
   }
-  // Check players distance again
-  distance = getDistance(playerX, playerY);
+
+  // Random chance to start from the south
+  if (Math.random() < 0.5) {
+    playerY = -playerY;
+  }
+
+  // Track number of steps taken to find the treasure
+  let steps = [];
+
+  // Initial distance from the treasure
+  let distance = getDistance(playerX, playerY);
+
+  // Game loop
+  while (distance !== 0) {
+
+    // Prompt player for move by selecting direction
+    let direction = prompt("You are " + distance + " meters away from the treasure. Will you go north, east, south, or west?");
+
+    // detects direction by taking the 0 index of the players input and then converts to lowercase
+    direction = direction[0].toLowerCase();
+
+    // Directional movement
+    if (direction === "n") {
+      playerY++;
+      steps.push("north")
+    } else if (direction === "s") {
+      playerY--;
+      steps.push("south")
+    } else if (direction === "e") {
+      playerX++;
+      steps.push("east")
+    } else if (direction === "w") {
+      playerX--;
+      steps.push("west")
+    } else {
+      alert("That's an invalid direction.")
+    }
+    // Check players distance again
+    distance = getDistance(playerX, playerY);
+  }
+
+  // Alert when treasure is found
+  alert("You found the " + treasure + "!");
+  //  Displays the full path taken 
+  alert("Your path was " + steps.join(" --> ") + ".");
 }
-
-// Alert when treasure is found
-alert("You found the " + treasure + "!");
-//  Displays the full path taken 
-alert("Your path was " + steps.join(" --> ") + ".");
-
 // 
 function getDistance(x, y) {
   // Calculates the current distance from the treasure
